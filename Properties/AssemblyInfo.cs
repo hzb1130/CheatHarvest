446afec19a321018b46bffc939215da854ff43d8
hzb1130 <626061157@qq.com>
2026-02-12 20:29:03 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using MelonLoader;
+using CheatHarvest;
+using System.Reflection;
+
+[assembly: AssemblyTitle("CheatHarvest")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+
+[assembly: MelonInfo(typeof(CheatHarvestMain), "CheatHarvest", "1.0.0", "HZB")]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
